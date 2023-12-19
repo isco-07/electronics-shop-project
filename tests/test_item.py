020a651b4b9e868f9e44c471c25d3354de57c052
Islam <islamka.1994@mail.ru>
2023-12-19 13:35:08 +0300

    fix tests/test_item.py file

--- tests/test_item.py
+++ tests/test_item.py
@@ -17,3 +17,23 @@ def test_apply_discount(item):
     item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 8000.0
+
+
+def test_name(item):
+    assert item.name == "Смартфон"
+
+
+def test_name_setter(item):
+    item.name = "СуперСмартфон"
+    assert item.name == "СуперСмарт"
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv("src/items.csv")
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number("5") == 5
+    assert Item.string_to_number("5.0") == 5
+    assert Item.string_to_number("5.5") == 5
