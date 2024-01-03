8fcf34417e633accf44104508edaf7bc47b045fc
Islam <islamka.1994@mail.ru>
2024-01-04 02:52:56 +0300

    fix src/keyboard.py, src/mixin.py and src/item.py files

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,20 +2,6 @@ from src.item import Item
 from src.mixin import MixinLog
 
 
-class Keyboard(Item):
-    def __init__(self, name: str, price: float, quantity: int):
-        super().__init__(name, price, quantity)
-        self.__language = 'EN'
-
-    @property
-    def language(self):
-        return self.__language
-
+class Keyboard(Item, MixinLog):
     def change_lang(self):
-        if self.__language == 'EN':
-            self.__language = 'RU'
-            return self.__language
-        else:
-            self.__language = 'EN'
-            return self.language
-
+        MixinLog.change_lang(self)
--- src/mixin.py
+++ src/mixin.py
@@ -1,10 +1,15 @@
 class MixinLog:
-
     def __init__(self):
-        self.__language = 'EN'
+        self.__language = "EN"
 
     @property
     def language(self):
         return self.__language
 
-
+    def change_lang(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+            return self.__language
+        else:
+            self.__language = "EN"
+            return self.language
